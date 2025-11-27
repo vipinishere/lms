47f8848f1241699d633b78588760470e3a40ca9d
vipin vishwakarma <vishwakarmavipin3434@gmail.com>
2025-11-28 01:06:28 +0530

    refactor: clean up Navbar component by removing unused educator data and improving layout

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { assets, dummyEducatorData } from '../../assets/assets';
+import { assets } from '../../assets/assets';
 import { UserButton, useUser } from '@clerk/clerk-react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const educatorData = dummyEducatorData;
   const { user } = useUser();
   return (
-    <div className="flex items-center justify-between px-4 md:px-8 border-b bg-gray-50 py-3">
+    <div
+      className="flex w-full  items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36
+      border-b border-gray-500 py-4 bg-cyan-100/70"
+    >
       <Link to={'/'}>
         <img src={assets.logo} alt="logo" className="w-28 lg:w-32" />
       </Link>
-      <div>
+      <div className="flex gap-1 items-center justify-around text-gray-500">
+        <Link to="/my-enrollments">Student Portal</Link>|
         <p>Hi {user ? user.fullName : 'Developers'}</p>
         {user ? <UserButton /> : <img className="max-w-8" src={assets.profile_img} />}
       </div>
